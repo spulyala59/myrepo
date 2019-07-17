8df492ef4862085d64efa4be008442d910d4baed
kpadala92 <kishore.zeta@gmail.com>
2019-07-18 00:16:57 +0530

    changing to world

--- src/main/java/com/test/HelloWorld.java
+++ src/main/java/com/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.test;
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("Hello World");
-	System.out.println("Welcome to Git universe");
+	System.out.println("Welcome to Git World");
 }
 
 }
