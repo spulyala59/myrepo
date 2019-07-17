09ab2ca51f776a1c6a47bd5ba451a9bd8dde776c
kpadala92 <kishore.zeta@gmail.com>
2019-07-18 00:09:34 +0530

    third commit

---
+++ src/main/java/com/test/Demo.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class Demo {
+public void methodOne() {
+	System.out.println("From method one");
+}
+}
--- src/main/java/com/test/HelloWorld.java
+++ src/main/java/com/test/HelloWorld.java
@@ -3,6 +3,7 @@ package com.test;
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("Hello World");
-	System.out.println("Welcome to Git world");
+	System.out.println("Welcome to Git universe");
 }
+
 }
